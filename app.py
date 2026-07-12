d165de76c58f69b3b27a9ba9879b2de01d257f3a
p-sh08 <01057811922s@gmail.com>
2026-07-13 01:01:45 +0900

    Clean up imports in app.py
    Removed unused imports from app.py.

--- app.py
+++ app.py
@@ -1,9 +1,3 @@
-import streamlit as st
-import pandas as pd
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.linear_model import LogisticRegression
-import re
-
 
 
 # =========================================================================
